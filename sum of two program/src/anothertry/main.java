cc89e4b33433d19671b1f14b90d62d16f9f43057
abdurahmanjabiin <jabiin626@gmail.com>
2023-01-01 22:51:26 +0300

    sum of 2 program in java

---
+++ sum of two program/src/anothertry/main.java
@@ -0,0 +1,24 @@
+package anothertry;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+  Scanner input = new Scanner(System.in);
+  int num1 = input.nextInt();
+  int num2 = input.nextInt();
+  
+  int sum = num1 + num2;
+  
+  System.out.println("sum =" + sum);
+		  
+
+		
+
+		
+					
+					
+	}
+
+}
